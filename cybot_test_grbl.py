0246c424688e7a58b85d60a4872325970857b36d
quartit <55748478+quartit@users.noreply.github.com>
2022-01-14 12:48:35 -0800

    Update cybot_test_grbl.py

--- cybot_test_grbl.py
+++ cybot_test_grbl.py
@@ -4,6 +4,9 @@ import time
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    time.sleep(0.1) #wait for serial to open
+    if arduino.isOpen():
+        print("{} connected!".format(arduino.port))
     ser.reset_input_buffer()
     ser.write("x30\n".encode('utf-8'))
     line = ser.readline().decode('utf-8').rstrip()
