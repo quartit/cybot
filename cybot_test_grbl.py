7fabaea2697627c27a28fd10da0e88f9b2056316
quartit <55748478+quartit@users.noreply.github.com>
2022-01-14 12:34:48 -0800

    Update cybot_test_grbl.py

--- cybot_test_grbl.py
+++ cybot_test_grbl.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+import serial
+import time
 
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+   
+        ser.write(b"x20\n")
+     
