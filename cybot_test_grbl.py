f2fee400e9ec82a08cdc02d1b382ad898ecfd181
quartit <55748478+quartit@users.noreply.github.com>
2022-01-14 12:43:48 -0800

    Update cybot_test_grbl.py

--- cybot_test_grbl.py
+++ cybot_test_grbl.py
@@ -5,5 +5,7 @@ import time
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
-    ser.write("x20\n")
+    ser.write(b"x20\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
      
