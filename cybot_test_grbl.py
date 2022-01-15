2364feedeb5f90f7007ac76a8d5d5dee523f5ba0
quartit <55748478+quartit@users.noreply.github.com>
2022-01-15 10:11:03 -0800

    Update cybot_test_grbl.py

--- cybot_test_grbl.py
+++ cybot_test_grbl.py
@@ -10,8 +10,10 @@ if __name__ == '__main__':
     
     print('Running. Press CTRL-C to exit.')
     with serial.Serial("/dev/ttyACM1", 115200, timeout=1) as arduino:
-        time.sleep(0.1) #wait for serial to open
+        time.sleep(2) #wait for serial to open
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
-            arduino.write("g91\n".encode('utf-8'))
+            #arduino.write("g91\n".encode('utf-8'))
             arduino.write("x50y100\n".encode('utf-8'))
+            arduino.write("x0y0\n".encode('utf-8'))
+            arduino.write("x-20y-800\n".encode('utf-8'))
