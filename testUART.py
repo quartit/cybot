580dcf940fdaf108160a9b2824c82c94a89c833b
quartit <55748478+quartit@users.noreply.github.com>
2022-02-05 22:16:14 +0100

    Create testUART.py

---
+++ testUART.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# lsusb to check device name
+#dmesg | grep "tty" to find port name
+
+import serial,time
+
+
+if __name__ == '__main__':
+    
+    print('Running. Press CTRL-C to exit.')
+    with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as stm32:
+        time.sleep(2) #wait for serial to open
+        if stm32.isOpen():
+            print("{} connected!".format(stm32.port))
+            stm32.write("g91\n".encode('utf-8'))
+           
