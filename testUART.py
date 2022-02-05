91c4b3731d7d07eaa4083b7c10cb8d8edc3a1bd8
quartit <55748478+quartit@users.noreply.github.com>
2022-02-06 00:10:00 +0100

    Update testUART.py

--- testUART.py
+++ testUART.py
@@ -3,7 +3,7 @@ import time
 import serial
 
 ser = serial.Serial(
-        port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
