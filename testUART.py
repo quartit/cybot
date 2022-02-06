8af0f00815dc368bdd0759d0f280689766b78110
quartit <55748478+quartit@users.noreply.github.com>
2022-02-06 12:09:24 +0100

    Update testUART.py

--- testUART.py
+++ testUART.py
@@ -15,4 +15,14 @@ if __name__ == '__main__':
         if stm32.isOpen():
             print("{} connected!".format(stm32.port))
             stm32.write("W".encode('utf-8'))
+     with serial.Serial("/dev/ttys0", 115200, timeout=1) as stm32:
+        time.sleep(2) #wait for serial to open
+        if stm32.isOpen():
+            print("{} connected!".format(stm32.port))
+            stm32.write("W".encode('utf-8'))
+     with serial.Serial("/dev/ttyS0", 115200, timeout=1) as stm32:
+        time.sleep(2) #wait for serial to open
+        if stm32.isOpen():
+            print("{} connected!".format(stm32.port))
+            stm32.write("W".encode('utf-8'))
            
