ad01f95da42b6fdea1d4b9a605e836605b2623b3
quartit <55748478+quartit@users.noreply.github.com>
2022-06-08 00:24:02 +0200

    Update cybot_test.py

--- cybot_test.py
+++ cybot_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
+import time
 from std_msgs.msg import String
 
 def talker():
@@ -12,6 +13,7 @@ def talker():
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
     rate.sleep()
+    time.sleep(2)
     hello_str = "MOVE<E2>4000</E2><E0>-15000</E0><E1>15000</E1>" 
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
