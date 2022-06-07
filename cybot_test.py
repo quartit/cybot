59b28fe5fec748085900588029c74b1fb17965ca
quartit <55748478+quartit@users.noreply.github.com>
2022-06-08 00:16:24 +0200

    Update cybot_test.py

--- cybot_test.py
+++ cybot_test.py
@@ -8,7 +8,11 @@ def talker():
     rospy.init_node('serial_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     #while not rospy.is_shutdown():
-    hello_str = "MOVE<E0>800</E0>" 
+    hello_str = "MOVE<E2>-4000</E2><E0>15000</E0>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    hello_str = "MOVE<E2>4000</E2><E0>-15000</E0>" 
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
     rate.sleep()
