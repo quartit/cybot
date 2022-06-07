d4f2490025c08783f14d33b2ed3ae418007dc6bc
quartit <55748478+quartit@users.noreply.github.com>
2022-06-08 00:05:25 +0200

    Update cybot_test.py

--- cybot_test.py
+++ cybot_test.py
@@ -1,1 +1,20 @@
-print('hello from git')
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('/cybot_btt_cmd', String, queue_size=10)
+    rospy.init_node('serial_node', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "MOVE<E0>800</E0>" 
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
