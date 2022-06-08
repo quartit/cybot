d9c1a5e6f35825b96a4c34b0d48734e6064e7ada
quartit <55748478+quartit@users.noreply.github.com>
2022-06-08 12:47:04 +0200

    Update cybot_test.py

--- cybot_test.py
+++ cybot_test.py
@@ -7,19 +7,67 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('/cybot_btt_cmd', String, queue_size=10)
     rospy.init_node('serial_node', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    #while not rospy.is_shutdown():
-    #hello_str = "MOVE<E2>-4000</E2><E0>15000</E0><E1>-15000</E1>" 
-    hello_str = "MOVE<X>-4000</X>" 
+    rate = rospy.Rate(10) # 10hz 
+    hello_str = "MOVE<X>2000</X>" 
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
     rate.sleep()
     time.sleep(2)
-    #hello_str = "MOVE<E2>4000</E2><E0>-15000</E0><E1>15000</E1>" 
-    hello_str = "MOVE<X>4000</X>" 
+    hello_str = "MOVE<X>-2000</X>" 
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
     rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<Y>2000</Y>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<Y>-2000</Y>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<Z>2000</Z>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<Z>-2000</Z>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E0>2000</E0>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E0>-2000</E0>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E1>2000</E1>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E1>-2000</E1>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E2>2000</E2>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
+    hello_str = "MOVE<E2>-2000</E2>" 
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    time.sleep(2)
 
 if __name__ == '__main__':
     try:
